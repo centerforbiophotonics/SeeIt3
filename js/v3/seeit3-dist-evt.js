a2d15a3ad89659583f971ce3d91cfa1c8dfd945c
matt-steinwachs <matt.steinwachs@gmail.com>
2012-05-09 14:55:53 -0700

    in dist v3: added parsing of comma seperated values to new worksheet.

--- js/v3/seeit3-dist-evt.js
+++ js/v3/seeit3-dist-evt.js
@@ -741,7 +741,15 @@ $('#loadFromForm').click(function(){
 	var rawText = $('#worksheetText').val();
 	var cells = [];
 	rawText.split('\n').forEach(function(line){
-		cells.push(line.split('\t'));
+		lineArray = [];
+		line.split('\t').forEach(function(tabChunk){
+			commaSep = tabChunk.split(",");
+			for (var i=0; i<commaSep.length; i++)
+				lineArray.push($.trim(commaSep[i]));
+		});
+		
+		
+		cells.push(lineArray);
 	});
 	if (validateWorksheetForm(title, cells)){
 		if (worksheetNew)
